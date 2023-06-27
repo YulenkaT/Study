51aafadc01a1340f49c018ddcb015f6d05ab581b
Yulenka <taninay@gmail.com>
2023-06-27 16:56:49 +0300

    1-int

---
+++ quarter_1/src/task1/task1.java
@@ -0,0 +1,16 @@
+package task1;
+// Написать программу, которая производит математические операции на двумя переменными a и b и вывести результаты на экран.
+//Т.е. должны быть выведены сумма, разность, произведение и частное. Попробуйте с разными типами данных a и b.
+public class task1 {
+
+        public static void main(String[] args) {
+
+                int a = 124;
+                int b = 12;
+
+                System.out.println(a+b);
+                System.out.println(a-b);
+                System.out.println(a*b);
+                System.out.println(a/b);
+        }
+}

f85bff35320ae85d5046dbd9814c121d9f57f3fc
Yulenka <taninay@gmail.com>
2023-06-28 13:33:58 +0300

    2.10

---
+++ quarter_1/src/tasks/Task_2_11.java
@@ -0,0 +1,21 @@
+package tasks;
+// Выведите на экран все положительные делители натурального числа, введённого пользователем с клавиатуры.
+
+import java.util.Scanner;
+
+public class Task_2_11 {
+    public static void main(String[] args) {
+        System.out.println("Введите натуральное число");
+        int m = 0;
+        Scanner n = new Scanner(System.in);
+        if(n.hasNextInt()) {
+            m = n.nextInt();
+        } else System.out.println("Введено неверное значение");
+        for (int c=1; c<m; c++) {
+            int r = m%c;
+            if (r == 0) {
+                System.out.println(c + " ");
+            }
+        }
+    }
+}

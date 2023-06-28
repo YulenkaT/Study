1e5aae4886d3ca7fc9c335483a1a3f81a617f34e
Yulenka <taninay@gmail.com>
2023-06-28 17:00:01 +0300

    3.1

---
+++ quarter_1/src/tasks/Task_3_1.java
@@ -0,0 +1,27 @@
+package tasks;
+// Создайте массив из пяти чисел и выведите их на экран в обратном порядке.
+
+import java.util.Arrays;
+
+public class Task_3_1 {
+        // Utility method to reverse elements of an array
+        public static void reverse(int[] nums)
+        {
+                int[] temp = new int[nums.length];
+
+                for (int i = 0; i < nums.length; i++) {
+                        temp[nums.length - 1 - i] = nums[i];
+                }
+
+                for (int i = 0; i < nums.length; i++) {
+                        nums[i] = temp[i];
+                }
+        }
+
+        public static void main(String[] args)
+        {
+                int[] nums = {1, 2, 3, 4, 5};
+                reverse(nums);
+                System.out.println(Arrays.toString(nums));
+        }
+}

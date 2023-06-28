e14db3b1c8ed0589202d6971e0eb10c69d9bc5cc
Yulenka <taninay@gmail.com>
2023-06-28 12:54:49 +0300

    2.6

---
+++ quarter_1/src/tasks/Task_2_6.java
@@ -0,0 +1,11 @@
+package tasks;
+// Создайте программу, выводящую на экран все четырёхзначные
+// числа последовательности 1000 1003 1006 1009 1012 1015 ….
+public class Task_2_6 {
+    public static void main(String[] args){
+        for(int a=1000;a<=9999;a=a+3){
+            System.out.print(a+" ");
+        }
+    }
+}
+

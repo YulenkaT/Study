f6cafbdb1c0c79e016431961da19d0e03d2cd6d8
Yulenka <taninay@gmail.com>
2023-06-28 13:09:47 +0300

    2.8

---
+++ quarter_1/src/tasks/Task_2_9.java
@@ -0,0 +1,9 @@
+package tasks;
+//Создайте программу, выводящую на экран первые 20 элементов последовательности 2 4 8 16 32 64 128 ….
+public class Task_2_9 {
+    public static void main(String[] args) {
+        for (int i = 0, a = 2; i < 20; a=a*2, i++) {
+            System.out.print(a + " ");
+        }
+    }
+}

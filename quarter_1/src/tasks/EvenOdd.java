cf4bbeaabf5d31715c34c781c0a5cbc0f570aecb
Yulenka <taninay@gmail.com>
2023-06-28 11:22:08 +0300

    2.2

--- quarter_1/src/tasks/EvenOdd.java
+++ quarter_1/src/tasks/EvenOdd.java
@@ -1,16 +1,12 @@
 package tasks;
 
-import java.util.Scanner;
-
 //Создать программу, проверяющую и сообщающую на экран, является ли целое число записанное
 // в переменную n, чётным либо нечётным.
 
 public class EvenOdd {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Введите значение n: ");
-        double n = scanner.nextDouble();
-        if (n%2 == 0) System.out.println(" n " + "четное число");
-        else System.out.println(" n " + "нечетное число");
+        int n = 3;
+        if (n%2 == 0) System.out.println("n " + "четное число");
+        else System.out.println("n " + "нечетное число");
     }
 }
---
+++ quarter_1/src/tasks/ten.java
@@ -0,0 +1,17 @@
+package tasks;
+// Создать программу, выводящую на экран ближайшее к 10 из двух чисел, записанных в переменные m и n.
+// Например, среди чисел 8 и 11 ближайшим к десяти будет 11.
+public class ten {
+    public static void main(String[] args) {
+        int m = 8;
+        int n = 11;
+        int a = 10;
+        int b1 = a - m;
+        int b2 = a - n;
+        if (b1 > b2) {
+            System.out.println(n);
+        } else {
+            System.out.println(m);
+        }
+    }
+}

e6ff77df54a19d4a9c09872c9bf3d1711720ae14
Yulenka <taninay@gmail.com>
2023-06-28 10:37:33 +0300

    2.1

--- quarter_1/src/tasks/EvenOdd.java
+++ quarter_1/src/tasks/EvenOdd.java
@@ -2,13 +2,15 @@ package tasks;
 
 import java.util.Scanner;
 
-//Создать программу, проверяющую и сообщающую на экран, является ли целое число записанное в переменную n, чётным либо нечётным.
+//Создать программу, проверяющую и сообщающую на экран, является ли целое число записанное
+// в переменную n, чётным либо нечётным.
+
 public class EvenOdd {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Введите значение a: ");
-        double a = scanner.nextDouble();
-        if (a%2 == 0) System.out.println(" a " + "четное число");
-        else System.out.println(" a " + "нечетное число");
+        System.out.print("Введите значение n: ");
+        double n = scanner.nextDouble();
+        if (n%2 == 0) System.out.println(" n " + "четное число");
+        else System.out.println(" n " + "нечетное число");
     }
 }

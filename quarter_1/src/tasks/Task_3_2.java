7914bca30c669d10d79f7ee22daf6cc18096938b
Yulenka <taninay@gmail.com>
2023-07-04 23:12:24 +0300

    3.5

--- quarter_1/src/tasks/Task_3_2.java
+++ quarter_1/src/tasks/Task_3_2.java
@@ -2,13 +2,20 @@ package tasks;
 // Создайте массив из семи элементов с помощью генератора случайных чисел и выведите его на экран.
 //  Увеличьте все его элементы в 2 раза.
 import java.util.Arrays;
+import java.util.Random;
 
 public class Task_3_2 {
-    public static void main(String[] args) {
-        int[] array = new int[7];
-        for (int i = 0; i < array.length; i++)
-            array[i] = (int) (Math.random() * (i));
+        public static void main(String[] args) {
+            int length = 7; // длина массива
+            int[] nums = new int[length];
 
-        System.out.print(Arrays.toString(array));
+            Random random = new Random(); // создаем объект класса Random
+
+            for (int i = 0; i < length; i++) {
+                // заполняем каждый элемент случайным числом от 0 до 99
+                nums[i] = random.nextInt(100);
     }
-}
+            // выводим массив на экран
+            System.out.println(Arrays.toString(nums));
+        }
+    }

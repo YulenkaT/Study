99101d3cf38743e1c716fb90f94618243ec07bb6
Yulenka <taninay@gmail.com>
2023-07-04 19:29:49 +0300

    3.5

--- quarter_1/src/tasks/Task_3_1.java
+++ quarter_1/src/tasks/Task_3_1.java
@@ -7,14 +7,14 @@ public class Task_3_1 {
         // Utility method to reverse elements of an array
         public static void reverse(int[] nums)
         {
-                int[] temp = new int[nums.length];
+                int[] a = new int[nums.length];
 
                 for (int i = 0; i < nums.length; i++) {
-                        temp[nums.length - 1 - i] = nums[i];
+                        a [nums.length - 1 - i] = nums[i];
                 }
 
                 for (int i = 0; i < nums.length; i++) {
-                        nums[i] = temp[i];
+                        nums[i] = a [i];
                 }
         }
 
